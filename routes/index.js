811b4a82d87e1781c63e293627fc5f0af1348514
Innocent Ingabire <inno100os@gmail.com>
2020-09-11 13:44:37 +0200

    tested getting courses and one course

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 // import Validate from '../middleware/courseValidate';
 // import express from 'express';
-import express from 'express';
+import express from "express";
 var router = express.Router();
-const classroomController = require('../controllers').classroom;
-const studentController = require('../controllers').student;
-const courseController = require('../controllers').course;
-const Lecturercontroller = require('../controllers').lecturer;
-const studentEnlorController = require('../controllers').studentEnrol;
-import { courseValidate } from '../middleware/courseValidate';
-
-
+const classroomController = require("../controllers").classroom;
+const studentController = require("../controllers").student;
+const courseController = require("../controllers").course;
+const Lecturercontroller = require("../controllers").lecturer;
+const studentEnlorController = require("../controllers").studentEnrol;
+import { courseValidate } from "../middleware/courseValidate";
+import { verify } from "jsonwebtoken";
+const verifyToken = require("../middleware/varifyToken");
 
 // import classroomController from '../controllers/classroom';
 // import studentController from '../controllers/student';
@@ -17,41 +17,41 @@ import { courseValidate } from '../middleware/courseValidate';
 // import Lecturercontroller from '../controllers/lecturer';
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 /*classroom Router*/
-router.get('/api/classrooms', classroomController.list);
-router.get('/api/classrooms/:id', classroomController.getById);
-router.post('/api/classrooms', classroomController.add);
-router.patch('/api/classrooms/:id', classroomController.update);
-router.delete('/api/classrooms/:id', classroomController.delete);
+router.get("/api/classrooms", classroomController.list);
+router.get("/api/classrooms/:id", classroomController.getById);
+router.post("/api/classrooms", classroomController.add);
+router.patch("/api/classrooms/:id", classroomController.update);
+router.delete("/api/classrooms/:id", classroomController.delete);
 /*student Router*/
-router.get('/api/students', studentController.list);
-router.get('/api/students/:id', studentController.getById);
-router.post('/api/students', studentController.add);
-router.patch('/api/students/:id', studentController.update);
-router.delete('/api/students/:id', studentController.delete);
+router.get("/api/students", studentController.list);
+router.get("/api/students/:id", studentController.getById);
+router.post("/api/students", studentController.add);
+router.patch("/api/students/:id", studentController.update);
+router.delete("/api/students/:id", studentController.delete);
 
 /*course Router*/
-router.get('/api/courses', courseController.list);
-router.get('/api/courses/:id', courseController.getById);
-router.post('/api/courses', courseValidate, courseController.add);
-router.patch('/api/courses/:id', courseController.update);
-router.delete('/api/courses/:id', courseController.delete);
+router.get("/api/courses", courseController.list);
+router.get("/api/courses/:id", courseController.getById);
+router.post("/api/courses", courseValidate, courseController.add);
+router.patch("/api/courses/:id", courseController.update);
+router.delete("/api/courses/:id", courseController.delete);
 
 /*Lecturer Router*/
-router.get('/api/lecturers', Lecturercontroller.list);
-router.get('/api/lecturers/:id', Lecturercontroller.getById);
-router.post('/api/lecturers', Lecturercontroller.add);
-router.patch('/api/lecturers/:id', Lecturercontroller.update);
-router.delete('/api/lecturers/:id', Lecturercontroller.delete);
+router.get("/api/lecturers", Lecturercontroller.list);
+router.get("/api/lecturers/:id", Lecturercontroller.getById);
+router.post("/api/lecturers", Lecturercontroller.add);
+router.patch("/api/lecturers/:id", Lecturercontroller.update);
+router.delete("/api/lecturers/:id", Lecturercontroller.delete);
 
 //student Enrol
 //router.post('/api/lecturers', studentEnrolcontroller);
-router.post('/api/courseEnrol', studentEnlorController.enrolCourse);
-router.get('/api/courseEnrol/:id', studentEnlorController.enroledStudent);
-router.get('/api/courseEnrol', studentEnlorController.listOfEnroled);
+router.post("/api/courseEnrol", studentEnlorController.enrolCourse);
+router.get("/api/courseEnrol/:id", studentEnlorController.enroledStudent);
+router.get("/api/courseEnrol", studentEnlorController.listOfEnroled);
 
 /*advanced rootes*/
 // router.post('/api/classrooms/add_with_students', classroomController.addWithStudents);
--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,21 @@
-<<<<<<< Updated upstream
-import express from 'express';
+const express = require("express");
 const router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
-});
-
-module.exports = router;
-=======
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/users');
-const passport = require('passport');
+const userController = require("../controllers/users");
+const passport = require("passport");
 require("../config/passport-setup");
 
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-router.get('/login', passport.authenticate('google', { failureRedirect: '/failed' }), userController.login);
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+router.get(
+  "/login",
+  passport.authenticate("google", { failureRedirect: "/failed" }),
+  userController.login
+);
 
 module.exports = router;
->>>>>>> Stashed changes
